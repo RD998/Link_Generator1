17258441fdb35a36a9a4af6f7171399b6c431155
Rachit Dheri <89472661+RD998@users.noreply.github.com>
2024-05-23 15:35:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const links = new Set();
+const allowedChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+
+function generate() {
+
+    additionalText=document.getElementById('Length').value;
+    while (links.size < additionalText) {
+      let identifier = "";
+      
+      for (let j = 0; j < 48; j++) {
+        for (let i=0; i < 5; i++) {
+        identifier += allowedChars[Math.floor(Math.random() * allowedChars.length)];
+      }
+    identifier=identifier+"-";
+    }
+      links.add(document.getElementById("input").value+ "11" + identifier);
+    }
+
+
+    const excelData = [[additionalText+' Links'], ...Array.from(links).map(link => [link])];
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.aoa_to_sheet(excelData);
+    XLSX.utils.book_append_sheet(workbook, worksheet, additionalText+" Links");
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const excelBlob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    const excelUrl = URL.createObjectURL(excelBlob);
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = excelUrl;
+    downloadLink.download = 'linksig.xlsx';
+    downloadLink.click();
+
+    document.body.removeChild(downloadLink);
+  }
