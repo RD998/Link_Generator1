501ae7d923768ba9bfe8fc9df833994710b367bd
Rachit Dheri <89472661+RD998@users.noreply.github.com>
2024-12-17 19:01:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-const links = new Set();
 const allowedChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
 
 function generate() {
+    
+    const links = new Set(); 
 
-    additionalText=document.getElementById('Length').value;
+    const additionalText = document.getElementById('Length').value;
     while (links.size < additionalText) {
-      let identifier = "";
-      
-      for (let j = 0; j < 8; j++) {
-        for (let i=0; i < 5; i++) {
-        identifier += allowedChars[Math.floor(Math.random() * allowedChars.length)];
-      }
-      console.log(j);
-      if (j!= 7){
-      identifier=identifier+"-";}
+        let identifier = "";
+        
+        for (let j = 0; j < 8; j++) {
+            for (let i = 0; i < 5; i++) {
+                identifier += allowedChars[Math.floor(Math.random() * allowedChars.length)];
+            }
+            if (j != 7) {
+                identifier += "-";
+            }
+        }
 
-    
-    }
-    
-      links.add(document.getElementById("input1").value+ identifier);
+        links.add(document.getElementById("input").value + identifier);
     }
 
-
-    const excelData = [[additionalText+' Links'], ...Array.from(links).map(link => [link])];
+    const excelData = [[additionalText + ' Links'], ...Array.from(links).map(link => [link])];
     const workbook = XLSX.utils.book_new();
     const worksheet = XLSX.utils.aoa_to_sheet(excelData);
-    XLSX.utils.book_append_sheet(workbook, worksheet, additionalText+" Links");
+    XLSX.utils.book_append_sheet(workbook, worksheet, additionalText + " Links");
     const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
     const excelBlob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
     const excelUrl = URL.createObjectURL(excelBlob);
@@ -33,7 +31,7 @@ function generate() {
     const downloadLink = document.createElement('a');
     downloadLink.href = excelUrl;
     downloadLink.download = 'linksig.xlsx';
+    document.body.appendChild(downloadLink);
     downloadLink.click();
-
-    document.body.removeChild(downloadLink);
-  }
+    document.body.removeChild(downloadLink); 
+}
